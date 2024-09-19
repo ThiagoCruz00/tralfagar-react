4dd35717918133a42ca5562981ad282257651028
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-09-18 22:51:20 -0300

    feat: Função para gerar a tela

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from './components/header'
+import SectionHome from './components/section-home'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <SectionHome />
+    </>
+  )
+}
