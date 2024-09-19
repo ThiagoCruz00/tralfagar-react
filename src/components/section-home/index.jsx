7efc6f99c10dc2eb68006986586aa817b6a5d7ef
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-09-18 22:52:13 -0300

    feat: Criação da estrutura da section

---
+++ src/components/section-home/index.jsx
@@ -0,0 +1,17 @@
+import './style.css'
+import Home from '../../assets/img/home.svg'
+
+export default function SectionHome() {
+    return (
+        <div className="section-home">
+            <section>
+                <div>
+                    <h1>Virtual healthcare for you</h1>
+                    <p>Trafalgar provides progressive, and affordable healthcare, accessible on mobile and online for everyone</p>
+                    <button>Consult today</button>
+                </div>
+                    <img src={Home} alt="Ilustração de sobre saúde" />
+            </section>
+        </div>
+    )
+}
