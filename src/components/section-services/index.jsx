40d250d61af4f9bbb14b503336fd17dfa714d4ad
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-09-27 19:49:47 -0300

    feat: Estrutura da page serviços

---
+++ src/components/section-services/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+export default function Services() {
+    return (
+        <>
+            <section className='servicos-container'>
+                <h1>Our services</h1>
+
+                <div className='borda'></div>
+                <p>We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors you can consult with us which type of service is suitable for your health</p>
+            </section>
+        </>
+    )
+}
