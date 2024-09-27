9eadd7af32a20fa0cedd8ffea2e3ddee56d0d146
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-09-27 19:49:11 -0300

    feat: Mudei para n uqebrar o código

--- src/components/section-home/index.jsx
+++ src/components/section-home/index.jsx
@@ -4,7 +4,7 @@ import Home from '../../assets/img/home.svg'
 export default function SectionHome() {
     return (
         <div className="section-home">
-            <section>
+            <section className='section-home'>
                 <div>
                     <h1>Virtual healthcare for you</h1>
                     <p>Trafalgar provides progressive, and affordable healthcare, accessible on mobile and online for everyone</p>
