492618f60ff4d5a7723c502502138f5548dc89f4
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-10-03 22:19:03 -0300

    feat: Estrtura da section-download

---
+++ src/components/section-download/download.jsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+export default function Download() {
+  return (
+    <section className="Download-container">
+      <div className="text-download">
+        <h1>Download our mobile apps</h1>
+        <div className="borda-accessible"></div>
+        <p>
+          Our dedicated patient engagement app and web portal allow you to
+          access information instantaneously (no tedeous form, long calls, or
+          administrative hassle) and securely
+        </p>
+        <button className="Download">Download <img src="" alt="" /></button>
+      </div>
+    </section>
+  );
+}
